8d6b327b7015a17c4c76fe343fa7fd0beacfe55c
KJEntytek303 <jakdom6@gmail.com>
2026-03-12 14:33:43 +0100

    Removed dead code.

--- src/main/java/net/brown_bakers/bakers_transfurs/init/InitRenderers.java
+++ src/main/java/net/brown_bakers/bakers_transfurs/init/InitRenderers.java
@@ -2,24 +2,16 @@ package net.brown_bakers.bakers_transfurs.init;
 
 import net.brown_bakers.bakers_transfurs.client.renderer.LatexFoxRenderer;
 
-import net.ltxprogrammer.changed.entity.ChangedEntity;
-
-import net.minecraft.world.entity.EntityType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static net.ltxprogrammer.changed.init.ChangedEntityRenderers.registerHumanoid;
 
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class InitRenderers {
-	
-	public static final List<EntityType<? extends ChangedEntity>> copyPlayerLayers = new ArrayList<>();
 	
 	@SubscribeEvent
 	public static void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
